6a9c7c6668ede5e71e40f937040ea08cc1f61a86
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-09-28 21:55:09 +0530

    changes in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://whiteanonymous:886ha-0048836@ds117423.mlab.com:17423/todoapp'
+  mlab: 'mongodb://TodoApp:886ha-0048836@ds117423.mlab.com:17423/todoapp'
 };
 mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 
