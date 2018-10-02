df017973274d47a1f3be60a19a9eb464925c47c8
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-10-02 18:02:23 +0530

    middleware GET /users/me

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -94,6 +95,25 @@ app.patch('/todos/:id', (req, res)=>{
     res.status(400).send();
   })
 });
+
+app.post('/users', (req,res)=>{
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(()=>{
+      return user.generateAuthToken();
+    }).then((token)=>{
+      res.header('x-auth', token).send(user);
+    }).catch((err)=>{
+      res.status(400).send(err);
+    });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
