6b9a0027d780bbeec4bf2e800bcd1244c0c6fb4d
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-10-12 22:09:05 +0530

    Add /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -299,3 +299,22 @@ describe('Post /users/login', ()=>{
   });
 
 });
+
+describe('Delete /users/me/token', ()=>{
+  it('Should be logout the user',(done)=>{
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth',users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res)=>{
+      if (err) {
+        return done(err);
+      }
+
+      User.findById(users[0]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+});
